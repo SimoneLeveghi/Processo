99b72d3468893820cac5b25c7f1b287f56ffb854
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-02-19 08:38:02 +0100

    Implement abstract methods and add overridden constructor and execute() method

--- src/ProcessoRicorrente.java
+++ src/ProcessoRicorrente.java
@@ -1,3 +1,23 @@
 public class ProcessoRicorrente extends Processo {
+    private int timesExecuted;
 
+    public ProcessoRicorrente(String nome) {
+        super(nome);
+        this.timesExecuted = 0;
+    }
+
+    public int getTimesExecuted() {
+        return this.timesExecuted;
+    }
+
+    @Override
+    protected void body() {
+        System.out.println("ProcessoRicorrente in sviluppo");
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+        this.timesExecuted++;
+    }
 }
