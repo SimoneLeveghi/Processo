534c9e9a18ab013ad1c1f938d408064ecf39ea4b
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2024-02-19 08:20:57 +0100

    Add boilerplate to class Processo

--- src/Processo.java
+++ src/Processo.java
@@ -1,3 +1,10 @@
 public abstract class Processo {
+    private String nome;
+    private boolean fatto;
+
+    public Processo(String nome) {
+        this.nome = nome;
+        this.fatto = false;
+    }
 
 }
